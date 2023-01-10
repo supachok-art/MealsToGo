71c8ca770f9183f1ff4512ce9225c128d365ee44
LAPTOP-4VMS1QG4\ASUS <artlovefamily095@gmail.com>
2023-01-10 13:58:39 +0700

    card

--- App.js
+++ App.js
@@ -1,45 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from
-"react-native";
-import { Searchbar } from "react-native-paper";
+import React from "react";
+import RestaurantsScreen from
+"./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const onChangeSearch = (query) => setSearchQuery(query);
-
-  return (
-  <>
-    <SafeAreaView style={{ flex: 1, marginTop:StatusBar.currentHeight }}>
-
-      <View style={styles.search}>
-        <Searchbar
-          placeholder="Search"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-        />
-      </View>
-      <View style={{ flex: 1, padding: 16, backgroundColor: "white"
-
-}}>
-
-        <Text>Art Supachok</Text>
-      </View>
-    </SafeAreaView>
-    <ExpoStatusBar style="auto" />
-    </>
-  );
+return (
+<>
+<RestaurantsScreen />
+<ExpoStatusBar style="auto" />
+</>
+);
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "	#e8e8e8",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  search: { 
-    padding: 16,
-    backgroundColor: "#79a867",
-
-  },
-});
